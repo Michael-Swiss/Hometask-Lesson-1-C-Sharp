417e4e94dc6401b115d827773871f9242925b682
Michael <michael_mosienko@mail.ru>
2023-04-26 21:48:56 +0300

    Добавил файл Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.WriteLine("Введите первое число a: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число b: ");
+// int b = int.Parse(Console.ReadLine());
+
+// if (a == b*b) {
+// Console.WriteLine("Yes");
+// } else {
+// Console.WriteLine("No");
+// } 
+
+Console.WriteLine("Введите первое число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) {
+Console.WriteLine("a - большее число и составляет: " + a + " а меньшее это: " + b);
+} else { 
+Console.WriteLine("b - большее число и составляет: " + b + " а меньшее это: " + a); 
+} 
+
